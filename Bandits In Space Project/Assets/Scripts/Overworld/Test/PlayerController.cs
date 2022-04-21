7f0c0ccd3bdf36dff59bcf11924edc6d95972064
Gustav Halvars <gustav--123@hotmail.com>
2022-04-21 13:59:36 +0200

    Overworld functionality done!

--- Bandits In Space Project/Assets/Scripts/Overworld/Test/PlayerController.cs
+++ Bandits In Space Project/Assets/Scripts/Overworld/Test/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
 
     public Transform movePoint;
     public Transform lastCheckpoint;
-    public Transform goalLevel;
+    public Transform levelGoal;
 
     public GameObject[] levels;
 
@@ -17,22 +17,30 @@ public class PlayerController : MonoBehaviour
 
     public string[] levelName;
 
+    private int lastCheckpointIndex;
+    private int levelGoalIndex;
+
     // Start is called before the first frame update
     void Start()
     {
         movePoint.parent = null;
         lastCheckpoint.parent = null;
+        levelGoal.parent = null;
         transform.position = levels[positionSelector].transform.position;
         movePoint.position = levels[positionSelector].transform.position;
         lastCheckpoint.position = levels[positionSelector].transform.position;
-        goalLevel.parent = null;
+        levelGoal.position = levels[positionSelector].transform.position;
+    }
 
+
+    void goTo(int indexToGoTo)
+    {
+        movePoint.position = levels[indexToGoTo].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
         for (int var1 = 0; var1 < levels.Length; var1++) //checks where the player last landed on a level for reference later
@@ -40,59 +48,42 @@ public class PlayerController : MonoBehaviour
             if (transform.position == levels[var1].transform.position)
             {
                 lastCheckpoint.position = levels[var1].transform.position;
+                lastCheckpointIndex = var1;
             }
         }
-
-        void goTo(int indexToGoTo)
+        if (Vector3.Distance(transform.position, levelGoal.position) < 0.1f)
         {
-            movePoint.position = levels[indexToGoTo].transform.position;
-        }
-
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-
-            if (hit)
+            if (Input.GetMouseButtonDown(0))
             {
-                for (int currentPositionLevel = 0; currentPositionLevel < levels.Length; currentPositionLevel++)
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+                if (hit)
                 {
-                    if (lastCheckpoint.position == levels[currentPositionLevel].transform.position)
+                    for (int goalLevel = 0; goalLevel < levels.Length; goalLevel++)
                     {
-                        for (int goalLevel = 0; goalLevel < levels.Length; goalLevel++)
+                        if (hit.transform.position == levels[goalLevel].transform.position)
                         {
-                            if (hit.transform.position == levels[goalLevel].transform.position)
-                            {
-                                Debug.Log(goalLevel - currentPositionLevel);
-                                int i = goalLevel - currentPositionLevel; //returns the difference between level "steps" i.e level 3 is two steps from level 1, returning 2
-                                if (i < 0)
-                                {
-                                    i *= -1;
-                                }
-                                if (goalLevel > currentPositionLevel)
-                                {
-                                    Debug.Log("Steps: " + i);
-                                    goTo(currentPositionLevel + 1);
-                                }
-                                else if (goalLevel < currentPositionLevel)
-                                {
-                                    Debug.Log("Steps: " + i);
-                                    goTo(currentPositionLevel - 1);
-                                }
-                            }
+                            levelGoal.position = levels[goalLevel].transform.position;
+                            levelGoalIndex = goalLevel;
                         }
                     }
                 }
             }
         }
 
+        int i = levelGoalIndex - lastCheckpointIndex; //returns the difference between level "steps" i.e level 3 is two steps from level 1, returning 2
 
-
-
-
-
-
-
-
+        if (i < 0)
+        {
+            i *= -1; //counts how many steps the player should take, if its -2 that means the player should take 2 steps backwards
+        }
+        if (levelGoalIndex > lastCheckpointIndex)
+        {
+            goTo(lastCheckpointIndex + 1);
+        }
+        else if (levelGoalIndex < lastCheckpointIndex)
+        {
+            goTo(lastCheckpointIndex - 1);
+        }
     }
 }
