9972567f3f4cc48992898903e1fe2f9ffe87af6a
Gustav Halvars <gustav--123@hotmail.com>
2022-04-28 19:36:21 +0200

    damage popup

--- Bandits In Space Project/Assets/Scripts/DiegoScripts/PlayerBandit.cs
+++ Bandits In Space Project/Assets/Scripts/DiegoScripts/PlayerBandit.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public abstract class PlayerBandit : MonoBehaviour, TileMovement
 {
+    public DamagePopup damagePopup;
     public TurnController turnController;
 
     public Image avatar;
@@ -92,5 +93,6 @@ public abstract class PlayerBandit : MonoBehaviour, TileMovement
     public void TakeDamage(int damageToTake)
     {
         healthPoints -= damageToTake;
+        damagePopup.Create(transform.position, damageToTake);
     }
 }
