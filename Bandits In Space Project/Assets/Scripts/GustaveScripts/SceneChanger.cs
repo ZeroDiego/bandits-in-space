48370161bfff9cf9cc06a8227a711a4ea66e5b59
gustave999 <gustavemillnersjoblom@gmail.com>
2022-04-28 08:28:27 +0200

    Fiender döda tar spelare till overworld
    Fixat kod för att ta spelaren till Overworld scenen när alla fiender i en scen är döda. Verkar vara buggfri... förhoppningsvis.

--- Bandits In Space Project/Assets/Scripts/GustaveScripts/SceneChanger.cs
+++ Bandits In Space Project/Assets/Scripts/GustaveScripts/SceneChanger.cs
@@ -7,18 +7,27 @@ public class SceneChanger : MonoBehaviour
 {
     [SerializeField] private FadeController fadeController;
     private bool isChangingScene;
-    public bool areEnemiesDead; 
+    
     public void ChangeScene()
     {
         fadeController.Fade();
         isChangingScene = true;       
     }
+ 
     private void Update()
     {
-        if (fadeController.imageToFade.color.a >= 1 && isChangingScene && areEnemiesDead)
+        if (fadeController.imageToFade.color.a >= 1 && isChangingScene)
         {
             SceneManager.LoadSceneAsync(gameObject.name);
+        } else if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0 && SceneManager.GetActiveScene().name != "Overworld")
+        {
+            ChangeSceneLevelComplete();
         }
+
     }
 
+    private void ChangeSceneLevelComplete()
+    {
+        SceneManager.LoadSceneAsync("Overworld");
+    }
 }
