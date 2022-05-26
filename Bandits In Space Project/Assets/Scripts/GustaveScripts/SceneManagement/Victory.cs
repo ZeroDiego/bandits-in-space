32249fa4e5f9c04c37de666c3d6fca57c9b53ab4
gustave999 <gustavemillnersjoblom@gmail.com>
2022-05-26 16:37:33 +0200

    victory screen

---
+++ Bandits In Space Project/Assets/Scripts/GustaveScripts/SceneManagement/Victory.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Victory : MonoBehaviour
+{
+    public static int numberOfLevelsComplete = 0;
+    [SerializeField] private SceneChanger sceneChanger;
+    [SerializeField] private AudioSource levelMusic;
+    [SerializeField] private AudioSource victoryMusic;
+    public GameObject hud; 
+    public GameObject victoryScreen;
+
+    private void Awake()
+    {
+        victoryMusic.enabled = false; 
+    }
+
+    private void increaseLevelsComplete()
+    {
+       
+        if (numberOfLevelsComplete == 0)
+        { 
+            hud.SetActive(false);
+            victoryScreen.SetActive(true); 
+            //sceneChanger.FadeToLevel(4); 
+        }
+        numberOfLevelsComplete++;
+        gameObject.SetActive(false); 
+    }
+
+    void Update()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+        if(GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
+        {
+            levelMusic.enabled = false;
+            victoryMusic.enabled = true; 
+            increaseLevelsComplete();             
+        }
+
+    }
+}
