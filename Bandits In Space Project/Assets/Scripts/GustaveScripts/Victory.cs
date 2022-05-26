e717f722c8ecd8ee897df30140f61d17f692d1bb
gustave999 <gustavemillnersjoblom@gmail.com>
2022-05-26 13:39:45 +0200

    vicotry music

--- Bandits In Space Project/Assets/Scripts/GustaveScripts/Victory.cs
+++ Bandits In Space Project/Assets/Scripts/GustaveScripts/Victory.cs
@@ -6,13 +6,22 @@ using UnityEngine.SceneManagement;
 public class Victory : MonoBehaviour
 {
     public static int numberOfLevelsComplete = 0;
-    [SerializeField] private SceneChanger sceneChanger; 
+    [SerializeField] private SceneChanger sceneChanger;
+    [SerializeField] private AudioSource levelMusic;
+    [SerializeField] private AudioSource victoryMusic;
+    GameObject pauseMenu;
+
+    private void Awake()
+    {
+        victoryMusic.enabled = false; 
+    }
 
     private void increaseLevelsComplete()
     {
         if(numberOfLevelsComplete == 0)
         {
-            sceneChanger.FadeToLevel(4); 
+            pauseMenu.SetActive(true); 
+            //sceneChanger.FadeToLevel(4); 
         }
         numberOfLevelsComplete++;
         gameObject.SetActive(false); 
@@ -24,6 +33,8 @@ public class Victory : MonoBehaviour
 
         if(GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
         {
+            levelMusic.enabled = false;
+            victoryMusic.enabled = true; 
             increaseLevelsComplete();             
         }
 
